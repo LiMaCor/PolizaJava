56bd834ecd737a0c04cc5e088e1546409f802601
PixelZer0 <pixelzero@gmail.com>
2017-01-18 12:34:35 +0100

    Añadidas variables y métodos constructores

---
+++ src/Modelo/ContratoDeSeguros.java
@@ -0,0 +1,42 @@
+package Modelo;
+
+/**
+ *
+ * @author PixelZer0
+ */
+public class ContratoDeSeguros {
+    
+    // Variables
+    
+    private int id;
+    private String fechaFirma;
+    private Persona persona;
+    private PolizaDeSeguros poliza;
+    private CompañiaDeSeguros compañia;
+    
+    /**
+     * Método constructor
+     * @param i - Representa el id del contrato
+     * @param f - Representa la fecha de firma del contrato
+     * @param p - Representa a la persona a la que se le hace el contrato
+     * @param poliza - Representa la póliza que figura en el contrato
+     * @param c - Representa la compañía que formaliza el ocntrato
+     */
+    
+    public ContratoDeSeguros(int i, String f, Persona p, PolizaDeSeguros poliza,
+                             CompañiaDeSeguros c) {
+        this.id = i;
+        this.fechaFirma = f;
+        this.persona = p;
+        this.poliza = poliza;
+        this.compañia = c;
+    }
+    
+    /**
+     * Constructor por defecto
+     */
+    
+    public ContratoDeSeguros() {
+    }
+    
+}
